d975ea1103e3cecd3929297f6421f807587f3ab4
Jonas <studentjonas@gmail.com>
2014-04-24 10:59:37 +0200

    Deleted all content of feature 1 and feature 2

--- features.c
+++ features.c
@@ -1,11 +1,4 @@
 void feature1(){
-    printf("This is feature 1\n");
-    printf("Change in feature 1\n");
-
 }
 void feature2(){
-    printf("This is feature 2\n");
-    printf("This is also in feature 2\n");
-    printf("Inserting the third line in the feature 2\n");
-
 }
